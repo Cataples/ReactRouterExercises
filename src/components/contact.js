2ed6f06ee712a724ad80bf81e80efff3c68129f0
Ples Catalin Andrei <catalin.ples@fortech.ro>
2018-08-09 11:18:06 +0300

    initial commit

---
+++ src/components/contact.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const Contact = () => {
+  return <p>CONTACT</p>;
+};
+
+export default Contact;
---
+++ src/components/home.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+const Home = () => {
+  return <p>This is dummy data for HOME page</p>;
+};
+
+export default Home;
